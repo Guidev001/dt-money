58260a1e975847bda9a04bf8fe7b7d76eefc3304
guilherme-david-yssy <guilherme.david@yssy.com.br>
2022-06-01 22:42:45 -0300

    Layout finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import { useState } from 'react';
+import Modal from 'react-modal'
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+import { NewTransactionModal } from './components/NewTransactionModal';
+import { GlobalStyle } from "./styles/global";
 
-function App() {
-  return <h1>Olá</h1>
+Modal.setAppElement('#root')
+
+export function App() {
+	const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+	function handleOpenNewTransactionModal() {
+		setIsNewTransactionModalOpen(true);
+	}
+
+	function handleCloseNewTransactionModal() {
+		setIsNewTransactionModalOpen(false);
+	}
+  return (
+    <>
+      <Header onOpenNewTransactionsModal={handleOpenNewTransactionModal} />
+
+      <Dashboard />
+
+      <NewTransactionModal  
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
+      <GlobalStyle />
+    </>
+  )
 }
 
-export default App;
